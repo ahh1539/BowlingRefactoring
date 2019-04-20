5494d6d6babdeef1036dae760c298650dec76eb2
Austin <ath9902@g.rit.edu>
2019-04-20 17:32:00 -0400

    Iterator Skeleton

--- src/Iterator.java
+++ src/Iterator.java
@@ -1,2 +1,34 @@
 public class Iterator {
+
+
+
+
+    public void first(){
+        next();
+    }
+
+    public void next() {
+
+    }
+
+    public boolean isDone(){
+        return value == -1;
+    }
+
+
+    public int currentValue(){
+        return value;
+    }
+
+    public void add(int in){
+        list.add(in);
+    }
+
+    public Iterator getIterator(){
+        return new Iterator(this);
+    }
+
+
+
+
 }
